278d1414d0e147a4ec9e093e960d07e6178332e1
Ricky Canepa <rickycanepa.rc@gmail.com>
2022-10-27 14:14:47 -0500

    general homepage layout

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>Sounds</h2>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>SoundStop</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/profile">Profile</a>
+                <a href="/search">Search</a>
+                <a href="/signout">Sign Out</a>
+
+            </div>
+        </nav>
+      );
+}
+ 
+export default Navbar;
