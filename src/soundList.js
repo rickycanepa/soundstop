8f679f864e9fa0b45e8963882a499c69cd4c1fa4
Ricky Canepa <rickycanepa.rc@gmail.com>
2022-10-28 17:10:37 -0500

    added child sound list component and props

---
+++ src/soundList.js
@@ -0,0 +1,19 @@
+const SoundList = (props) => {
+    const sounds = props.sounds
+    return (
+        <div className="sound-list">
+            {sounds.map((sound) => (
+                <div className="sound-preview" key={ sounds.id }>
+                    <h3>{ sound.title }</h3>
+                    <p>Produced by: { sound.artist }</p>
+                    <p>Genre: { sound.genre }</p>
+                    <p>Key: { sound.key }</p>
+                    <p>BPM: { sound.bpm }</p>
+                </div>
+                
+            ))}
+        </div>
+      );
+}
+ 
+export default SoundList;
